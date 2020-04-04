5e6b25098555c3e72deef4dfb149a2540d2a8936
sunilkumar1996 <sunilrajputdev@gmail.com>
2020-04-04 14:20:36 +0530

    Add new route with id then return one response

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ app = Flask(__name__)
 
 books = [
 {
+	"id": 1,
 	"name": "python",
 	"price": 1500,
 	"Number": 123456789
 },
 {
+	"id": 2,
 	"name":"Ruby",
 	"price": 1600,
 	"Number": 123123123
@@ -19,4 +21,19 @@ books = [
 def hello():
 	return jsonify({"books":books})
 
-app.run(port=5001)
+
+@app.route('/books/<int:id>')
+def get_book(id):
+	book_arry = {}
+	for book in books:
+		if book['id'] == id:
+			book_arry = {
+			'name': book['name'],
+			'price': book['price'],
+			'Number': book['Number']
+			}
+			return book_arry
+		return "Not Found" 
+
+if __name__=="__main__":
+	app.run(debug=True)
