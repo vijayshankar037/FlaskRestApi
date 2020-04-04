b3d09f24aa6fe2bf36f4081393a7fa1ee7e8f903
vijayshankar037 <vijayshankar1990@hotmail.com>
2020-04-04 13:58:03 +0530

    Added static books content

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask
 app = Flask(__name__)
 
+books = [
+    {
+        "name":"Progamming in C",
+        "author": "Denis Rich",
+        "isbn": 12345001
+    },
+    {
+        "name":"Python3",
+        "author": "Matt",
+        "isbn":12345002
+    }
+]
+
 @app.route('/')
 def hello_world():
     return "Hello world"
+
+@app.route('/books')
+def get_books():
+    return {
+                "books": [
+                            {"status": 200}
+                ]
+           }
 
 app.run(port =5000)
