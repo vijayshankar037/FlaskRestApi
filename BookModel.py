856a5105ff461a5be197a7781b587c78632abd9d
vijayshankar037 <vijayshankar1990@hotmail.com>
2020-04-05 11:25:18 +0530

    Delete query

---
+++ BookModel.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import json
+from setting import app
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    __tablename__ = 'books' #table name with schema details
+    id = db.Column(db.Integer, primary_key=True) #table field id as PK
+    name = db.Column(db.String(80), nullable=False) #table field name
+    author = db.Column(db.String(80), nullable=False) #table field author
+    isbn = db.Column(db.Integer) #table field isbn
+
+    def __init__(**kwargs):
+        super(Foo, self).__init__(**kwargs)
+
+    #Model method to insert new record in db
+    def add_book(_name, _author, _isbn):
+        new_book = Book(name= _name, author= _author, isbn = _isbn)
+        db.session.add(new_book)
+        db.session.commit()
+
+    #Model method to fetch all records
+    def get_all_books():
+        return Book.query.all()
+
+    #Get book with isbn
+    def get_book(_isbn):
+        return Book.query.filter_by(isbn=_isbn).first()
+
+    #Delete book with isbn
+    def delete_book(_isbn):
+        Book.query.filter_by(isbn=_isbn).delete()
+        db.session.commit()
+
+    #Defining representation of Model object
+    def __repr__(self):
+        book_object = {
+            'id': self.id,
+            'name': self.name,
+            'author': self.author,
+            'isbn': self.isbn
+        }
+        return json.dumps(book_object)
--- setting.py
+++ setting.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATBASE_URI'] = "sqlite:///Users/vijay/workspace/python/FlaskRestApi/database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/vijay/workspace/python/FlaskRestApi/database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
